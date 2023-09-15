302d6d279980a2926ad3c79965849e3aa6f6a9c7
Shweta Nazare <nshweta90@gmail.com>
2023-09-15 21:05:22 +0200

    mapper file update

--- src/main/java/com/game/mancala/datamapper/GameDataMapper.java
+++ src/main/java/com/game/mancala/datamapper/GameDataMapper.java
@@ -1,17 +1,14 @@
 package com.game.mancala.datamapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import com.game.mancala.datamodel.GameDataEntity;
 import com.game.mancala.model.Game;
 
-@Mapper(componentModel = "spring") 
-public interface GameDataMapper {  
-	
+@Mapper(componentModel = "spring" ,unmappedTargetPolicy = ReportingPolicy.IGNORE)  
+public interface GameDataMapper {   	
 	 
-	public GameDataEntity mapBOToEntity(Game bo);
-	
-	 
-	public Game mapEntityToBO(GameDataEntity entity);
-
+	public GameDataEntity mapBOToEntity(Game bo); 		
+	public Game mapEntityToBO(GameDataEntity entity);	
 }
--- src/test/java/com/game/mancala/integration/PlayerControllerTests.java
+++
@@ -1,5 +0,0 @@
-package com.game.mancala.integration;
-
-public class PlayerControllerTests {
-
-}
